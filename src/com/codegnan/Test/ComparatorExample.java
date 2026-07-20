3a16636144bcbc382efbbec50597dfcee4629445
Purnimaaa <purnimamungara@gmail.com>
2026-07-20 19:54:31 +0530

    feat:Comaparator Example

---
+++ src/com/codegnan/Test/ComparatorExample.java
@@ -0,0 +1,17 @@
+package com.codegnan.Test;
+
+public class ComparatorExample {
+ String name;
+int id;
+public ComparatorExample(String name, int id) {
+	super();
+	this.name = name;
+	this.id = id;
+}
+@Override
+public String toString() {
+	return "ComparatorExample [name=" + name + ", id=" + id + "]";
+}
+
+}
+
